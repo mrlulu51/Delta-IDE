eab0304ddd7482bf779ef3a824452fda57f2a968
Mr-lulu-51 <lulubinou51@gmail.com>
2021-08-21 13:32:34 +0200

    Declared the options window

--- src/main/java/fr/delta/ide/overlay/OptionWindow.java
+++ src/main/java/fr/delta/ide/overlay/OptionWindow.java
@@ -1,10 +1,21 @@
 package fr.delta.ide.overlay;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import static fr.delta.ide.util.Graphics.StageFactory.*;
 
 public class OptionWindow {
 
-    public OptionWindow() {
+    private Scene optionScene;
 
+    public OptionWindow() {
+        Stage options = makeStage(new Stage())
+                .title("Preferences")
+                .setWidth(800)
+                .setHeight(600)
+                .scene(this.optionScene)
+                .show()
+                .get();
     }
 }
